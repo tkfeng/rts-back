2c87393068c79ef8ef3e4b05163321555bac90d1
TK Feng <tk.feng@gmail.com>
2020-09-22 16:58:14 -0700

    Add models
    Refactored messages and users entity to the models folder and imported accordingly.

--- src/index.js
+++ src/index.js
@@ -2,30 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import { v4 as uuidv4 } from 'uuid';
-
-let users = {
-  1: {
-    id: '1',
-    username: 'Robin Wieruch',
-  },
-  2: {
-    id: '2',
-    username: 'Dave Davids',
-  },
-};
- 
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2',
-  },
-};
+import models from './models';
 
 const app = express();
 
@@ -39,28 +16,31 @@ app.get('/', (req, res) => {
 });
 
 app.use((req, res, next) => {
-  req.me = users[1];
+  req.context = {
+    models,
+    me: models.users[1],
+  };
   next();
 });
 
 app.get('/session', (req, res) => {
-  return res.send(users[req.me.id]);
+  return res.send(req.context.models.users[req.context.me.id]);
 });
  
 app.get('/users', (req, res) => {
-  return res.send(Object.values(users));
+  return res.send(Object.values(req.context.models.users));
 });
  
 app.get('/users/:userId', (req, res) => {
-  return res.send(users[req.params.userId]);
+  return res.send(req.context.models.users[req.params.userId]);
 });
 
 app.get('/messages', (req, res) => {
-  return res.send(Object.values(messages));
+  return res.send(Object.values(req.context.models.messages));
 });
  
 app.get('/messages/:messageId', (req, res) => {
-  return res.send(messages[req.params.messageId]);
+  return res.send(req.context.models.messages[req.params.messageId]);
 });
 
 app.post('/messages', (req, res) => {
@@ -68,10 +48,10 @@ app.post('/messages', (req, res) => {
   const message = {
     id,
     text: req.body.text,
-    userId: req.me.id,
+    userId: req.context.me.id,
   };
  
-  messages[id] = message;
+  req.context.models.messages[id] = message;
  
   return res.send(message);
 });
@@ -80,9 +60,10 @@ app.delete('/messages/:messageId', (req, res) => {
   const {
     [req.params.messageId]: message,
     ...otherMessages
-  } = messages;
+  } = req.context.models.messages;
+
  
-  messages = otherMessages;
+  req.context.models.messages = otherMessages;
  
   return res.send(message);
 });
